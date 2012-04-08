d7bb244ebefacc3c63e1b312bcd01e5fced705bb
Matt Carter <m@ttcarter.com>
2012-04-08 11:11:02 +1000

    General code cleanup Added test cases for RE Updated RE documentation

--- helpers/pherl_helper.php
+++ helpers/pherl_helper.php
@@ -1,5 +1,4 @@
-<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-
+<?php
 if (!function_exists('qw')) {
 /**
 * Quickly initalize arrays by providing a single string. The array elements are determined by any whitespace.
@@ -34,11 +33,18 @@ if (!function_exists('re')) {
 *
 *	list($name,$age,$height) = re('m/([a-z]+) is ([0-9]{1,3}) years old and is ([0-9\.]+) feet height',$instr); // Nice extraction
 *
-* Operators:
+* Operators (characters that go before the expression):
 * * m (or nothing) - Returns all matching elements in a array (if multiple matches [i.e. \1\2\3 etc.] in an array of arrays)
 * * s - Return the substution of the RegExp (or the changed array if $target is array)
 * * tr - Inline translate (change each character to its corrisponding character)
 * * ! - Boolean match return yes or no as to whether the subject is within the target (or in ANY of the targets if its an array)
+*
+* Modifiers (characters that go after the expression):
+* * i - Case insensitive matching
+* * g - Subsitute globally - i.e. dont just replace in an s/// operation once
+* * s - Treat the incomming target as a string. Change "." to match any character (even a newline)
+* * m - Treat string as multiple lines. Change "^" and "$" from matching the start or end of the string to matching the start or end of any line anywhere within the string.
+* * @ - Always return the result as an array even if there is only one return (e.g. $a = re('/f(.)/@') would return $a as an array)
 *
 * Flags:
 * * RE_ALWAYSARRAY - Ignore the single return (when doing simple matches) and ALWAYS return an array-of-arrays
@@ -49,45 +55,50 @@ define('RE_ALWAYSARRAY',1);
 define('RE_FULLSTR',2);
 define('RE_CROP',4);
 function re($exp, $target, $flags = 0) {
-	preg_match_all('/^(m|s|tr|\!)?([^a-z0-9])/',$exp, $type, PREG_SET_ORDER); // Determine what type of operation to do & the split char
-	$splitter = $type[0][2];
-	$type = $type[0][1];
-	$exp = ltrim($exp,$type); // Remove the leading operation character (PHP PREG lib doesnt like it)
+	preg_match_all('/^(m|s|tr|\!)?([^a-z0-9])/', $exp, $function, PREG_SET_ORDER); // Determine what type of operation to do & the split char
+	$splitter = $function[0][2];
+	$splitterq = preg_quote($splitter, $splitter);
+	$type = $function[0][1];
+	$exp = ltrim($exp, $type); // Remove the leading operation character (PHP PREG lib doesnt like it)
+
+	preg_match("/{$splitterq}([igsm@]*)$/i", $exp, $mutators);
+	$mutators = $mutators[1];
+	if (strpos($mutators, '@') !== FALSE) {
+		$flags = $flags & RE_ALWAYSARRAY;
+	}
 	
 	switch ($type) { // What exactly are we doing?
 		case 's': // Substitution
-			$splitterq = preg_quote($splitter,$splitter);
-			preg_match_all("/^$splitterq(.*)$splitterq(.*)[$splitterq]$/i",$exp,$matches,PREG_SET_ORDER);
+			preg_match_all("/^$splitterq(.*)$splitterq(.*)[$splitterq]$/i", $exp, $matches,PREG_SET_ORDER);
 			if (is_array($target)) {
 				$out = array();
 				foreach ($target as $thistarget)
-					$out[] = preg_replace("$splitter{$matches[0][1]}$splitter",$matches[0][2],$target);
+					$out[] = preg_replace("$splitter{$matches[0][1]}$splitter", $matches[0][2], $target);
 				return $out;
 			} else
-				return preg_replace("$splitter{$matches[0][1]}$splitter",$matches[0][2],$target);
+				return preg_replace("$splitter{$matches[0][1]}$splitter", $matches[0][2], $target);
 			print_r($matches);
 			break;
 		case 'tr': // Translation FIXME: Does this work with tr/a-z/A-Z/
-			$splitter = preg_quote($splitter,$splitter);
-			preg_match_all("/^$splitter(.*)$splitter(.*)[$splitter]$/i",$exp,$matches,PREG_SET_ORDER);
+			preg_match_all("/^$splitterq(.*)$splitterq(.*)[$splitterq]$/i", $exp, $matches, PREG_SET_ORDER);
 			if (is_array($target)) {
 				$out = array();
 				foreach ($target as $thistarget)
-					$out[] = strtr($thistarget,$matches[0][1],$matches[0][2]);
+					$out[] = strtr($thistarget, $matches[0][1], $matches[0][2]);
 				return $out;
 			} else
-				return strtr($target,$matches[0][1],$matches[0][2]);
+				return strtr($target, $matches[0][1], $matches[0][2]);
 			break;
 
 		case '!': // Boolean match
 			if (is_array($target)) {
 				foreach ($target as $thistarget) {
-					if (preg_match($exp,$thistarget) > 0)
+					if (preg_match($exp, $thistarget) > 0)
 						return true;
 				}
 				return false;
 			} else
-				return (preg_match($exp,$target) > 0);
+				return (preg_match($exp, $target) > 0);
 			break;
 
 		case 'm':
@@ -95,10 +106,10 @@ function re($exp, $target, $flags = 0) {
 			if (is_array($target)) {
 				$out = array();
 				foreach ($target as $thistarget) {
-					preg_match_all($exp,$thistarget,$matches,PREG_SET_ORDER);
+					preg_match_all($exp, $thistarget, $matches, PREG_SET_ORDER);
 					if (($flags & RE_FULLSTR) == RE_FULLSTR) // Have to go though and put the thing in
 						foreach ($matches as $index => $match)
-							array_unshift($matches[$index],$thistarget);
+							array_unshift($matches[$index], $thistarget);
 					if (isset($matches[0]))
 						$out[] = $matches[0];
 					else if (($flags & RE_CROP) != RE_CROP) { // Nothing found in this element (and we're not in CROP mode)
@@ -110,7 +121,7 @@ function re($exp, $target, $flags = 0) {
 				}
 				return $out;
 			} else {
-				preg_match_all($exp,$target,$matches,PREG_SET_ORDER);
+				preg_match_all($exp, $target, $matches, PREG_SET_ORDER);
 				if ((($flags & RE_ALWAYSARRAY) == RE_ALWAYSARRAY) || (count($matches) > 1)) { // If its multiple groups return the default array-of-arrays
 					if (($flags & RE_FULLSTR) != RE_FULLSTR) // Have to go though and remove the damned thing
 						foreach ($matches as $index => $match)
@@ -128,10 +139,32 @@ function re($exp, $target, $flags = 0) {
 }
 }
 
+/*
 // Test cases:
-//print_r(re('m/([1-3])/','123456789123456789'));
-//print_r(re('m/([1-3])/','123456789123456789',RE_ALWAYSARRAY));
-//print_r(re('m/([1-3])[0-9]([1-3])/','123456789123456789'));
-//print_r(re('/567/','123456789'));
-//print_r(re('s/[5-9]/-R-/','123456789'));
-//print_r(re('tr/1-9/a-z/','123456789'));
+function testre($title, $expression, $target, $expected) {
+	$result = re($expression, $target, $expected);
+	if ($result == $expected) {
+		echo "Test '$title' [$expression] - OK\n";
+	} else {
+		echo "Test '$title' [$expression] - FAILED\n";
+		echo "Expected: [" . print_r($expected, 1) . "]\n";
+		echo "Got: [" . print_r($result, 1) . "]\n";
+		echo "\n";
+	}
+}
+testre('Boolean match', 'm/foo/@i','foo bar baz', 1);
+die();
+testre('Boolean match', 'm/foo/','foo bar baz', 1);
+testre('Boolean match (implied function)', '/foo/','foo bar baz', 1);
+testre('Boolean match (case insensitive)', 'm/fOo/i','foO bar baz', TRUE);
+testre('Single capture', 'm/(fo.)/','foo bar baz', 'foo');
+testre('Single capture as array', 'm/(fo.)/@','foo bar baz', array('foo'));
+testre('Multiple capture', 'm/(ba.)/','foo bar baz', qw('bar baz'));
+
+testre('Single subsitution', 's/ba./QUZ/', 'foo bar baz', 'foo QUZ baz');
+testre('Multiple subsitution', 's/ba./QUZ/g', 'foo bar baz', 'foo QUZ QUZ');
+testre('Single subsitution (un-closed splitter)', 's/ba./QUZ', 'foo bar baz', 'foo QUZ baz');
+
+testre('Translation of a single char', 'tr/a/A/','foo bar baz', 'foo bAr bAz');
+testre('Translation to upper case', 'tr/a-z/A-Z/','foo bar baz', 'FOO BAR BAZ');
+*/
